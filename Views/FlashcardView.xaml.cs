6f1ea5e31854d17c7490d500288c1846a25d06de
amirschool2 <amirenglishschool@gmail.com>
2026-02-25 18:47:54 +0330

    Create FlashcardView.xaml.cs

---
+++ Views/FlashcardView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class FlashcardView : UserControl
+    {
+        public FlashcardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
