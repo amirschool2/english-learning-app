ce980ea661b85ba716a3a0554d7a27e71e514f52
amirschool2 <amirenglishschool@gmail.com>
2026-02-25 18:48:41 +0330

    Create QuizView.xaml.cs

---
+++ Views/QuizView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class QuizView : UserControl
+    {
+        public QuizView()
+        {
+            InitializeComponent();
+        }
+    }
+}
