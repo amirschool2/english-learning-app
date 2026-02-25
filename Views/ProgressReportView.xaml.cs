2079c12afd45bb8f7917130d87ea21b7adefcb03
amirschool2 <amirenglishschool@gmail.com>
2026-02-25 18:50:07 +0330

    Create ProgressReportView.xaml.cs

---
+++ Views/ProgressReportView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class ProgressReportView : UserControl
+    {
+        public ProgressReportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
