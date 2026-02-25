283264fbb8f12e001f9428b731c1735c6f7794d0
amirschool2 <amirenglishschool@gmail.com>
2026-02-25 18:49:24 +0330

    Create PronunciationView.xaml.cs

---
+++ Views/PronunciationView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class PronunciationView : UserControl
+    {
+        public PronunciationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
